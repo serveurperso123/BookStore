c5414c3662ca579ac0694c95a67b188ae31f8ec4
myris ebogo <ebogomyris@yahoo.com>
2023-02-10 22:29:30 +0100

    Created enities and BookStoreContext

---
+++ src/BookStore.Infrastructure/Mappings/CategoryMapping.cs
@@ -0,0 +1,28 @@
+﻿using BookStore.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookStore.Infrastructure.Mappings
+{
+    public class CategoryMapping : IEntityTypeConfiguration<Category>
+    {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            builder.HasKey(c => c.Id);
+
+            builder.Property(c => c.Name)
+                .IsRequired()
+                .HasColumnType("varchar(150)");
+
+            // 1:N=> Category : Books
+            builder.HasMany(c => c.Books)
+                .WithOne(b => b.Category)
+                .HasForeignKey(b => b.Id);
+
+            builder.ToTable("Categories");
+        }
+    }
+}
---
+++ src/BookStore.Infrastructure/context/BookStoreDbContext.cs
@@ -0,0 +1,32 @@
+﻿using BookStore.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BookStore.Infrastructure.context
+{
+    public class BookStoreDbContext : DbContext
+    {
+        public BookStoreDbContext(DbContextOptions options) : base(options) { }
+
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            foreach (var property in modelBuilder.Model.GetEntityTypes()
+                .SelectMany(e => e.GetProperties()
+                    .Where(p => p.ClrType == typeof(string))))
+                property.SetColumnType("varchar(150)");
+
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(BookStoreDbContext).Assembly);
+
+            foreach (var relationship in modelBuilder.Model.GetEntityTypes()
+                .SelectMany(e => e.GetForeignKeys())) relationship.DeleteBehavior = DeleteBehavior.ClientSetNull;
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
