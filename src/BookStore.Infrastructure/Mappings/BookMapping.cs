b94d4018c914e6c98812f7f846847ceaec9bcb96
myris ebogo <ebogomyris@yahoo.com>
2023-05-02 14:50:36 +0200

    creation of entities and contexr

---
+++ src/BookStore.Infrastructure/Mappings/BookMapping.cs
@@ -0,0 +1,42 @@
+﻿using BookStore.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStore.Infrastructure.Mappings
+{
+    public class BookMapping : IEntityTypeConfiguration<Book>
+    {
+        public void Configure(EntityTypeBuilder<Book> builder)
+        {
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.Name)
+                .IsRequired()
+                .HasColumnType("Varchar(150)");
+
+            builder.Property(x => x.Description)
+                .HasColumnType("varchar(350)");
+
+            builder.Property(x => x.Author)
+                .IsRequired()
+                .HasColumnType("varchar(150)");
+
+            builder.Property(b => b.Value)
+                .IsRequired();
+
+            builder.Property(b => b.PublishDate)
+                .IsRequired();
+
+            builder.Property(b => b.CategoryId)
+                .IsRequired();
+
+            builder.ToTable("Books");
+
+        }
+    }
+}
